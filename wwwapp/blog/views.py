e70068427ddda7783f122c8584ac74397d013707
Jacek Ruszkowski <jac.ruszkowski@gmail.com>
2020-09-14 18:05:45 +0200

    django_start_1

--- wwwapp/blog/views.py
+++ wwwapp/blog/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.shortcuts import render
+# from django.shortcuts import render
 
-def test(request, imie, wiek):
-    template_data = {
-        't_imie': imie,
-        't_wiek': wiek,
-    }
-    return render(request, 'blog/test.html', template_data)
+# def test(request, imie, wiek):
+#     template_data = {
+#         't_imie': imie,
+#         't_wiek': wiek,
+#     }
+#     return render(request, 'blog/test.html', template_data)
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
